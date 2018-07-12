4bb2267db5b24d6326075ddd22d8ad2c6275a5c7
Grace Vanessa <graceace49@gmail.com>
2018-07-13 00:18:01 +0700

    Budiman's GUI

---
+++ src/View/AddOrEdit.java
@@ -0,0 +1,134 @@
+package View;
+
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.WindowConstants;
+
+public class AddOrEdit extends JFrame {
+
+    public AddOrEdit() {
+        initComponents();
+    }
+
+    private void onClickSubmit(ActionEvent evt) {
+        if (evt.toString().equalsIgnoreCase("submit")) {
+            JOptionPane.showMessageDialog(null, "Berhasil di submit");
+        } else {
+            JOptionPane.showMessageDialog(null, "Gagal di submit");
+        }
+    }
+
+    private void onClickCancel(ActionEvent evt) {
+        if (evt.toString().equalsIgnoreCase("submit")) {
+            JOptionPane.showMessageDialog(null, "Berhasil di cancel");
+        }
+    }
+
+    private void initComponents() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setSize(700, 400);
+        this.setLocationRelativeTo(null);
+
+        setTitle("Add or Edit Buku");
+
+        getContentPane().setLayout(null);
+
+        pnlPanel1 = new JPanel();
+        pnlPanel1.setSize(700, 400);
+        add(pnlPanel1);
+
+        lblLabelKodeBuku = new JLabel("Kode Buku : ");
+        pnlPanel1.add(lblLabelKodeBuku);
+        txtTextFieldKodeBuku = new JTextField(10);
+        pnlPanel1.add(txtTextFieldKodeBuku);
+
+        lblLabelJudulBuku = new JLabel("Judul Buku : ");
+        pnlPanel1.add(lblLabelJudulBuku);
+        txtTextFieldJudulBuku = new JTextField(50);
+        pnlPanel1.add(txtTextFieldJudulBuku);
+
+        lblLabelNamaPengarang = new JLabel("Nama Pengarang : ");
+        pnlPanel1.add(lblLabelNamaPengarang);
+        txtTextFieldNamaPengarang = new JTextField(20);
+        pnlPanel1.add(txtTextFieldNamaPengarang);
+
+        lblLabelNamaPenerbit = new JLabel("Nama Penerbit : ");
+        pnlPanel1.add(lblLabelNamaPenerbit);
+        txtTextFieldJNamaPenerbit = new JTextField(20);
+        pnlPanel1.add(txtTextFieldJNamaPenerbit);
+
+        lblLabelTahunTerbit = new JLabel("Tahun Terbit : ");
+        pnlPanel1.add(lblLabelTahunTerbit);
+        txtTextFieldTahunTerbit = new JTextField(4);
+        pnlPanel1.add(txtTextFieldTahunTerbit);
+
+        lblLabelFiksiAtauNon = new JLabel("Fiksi/Non-Fiksi : ");
+        pnlPanel1.add(lblLabelFiksiAtauNon);
+        Object[] arrPilih1 = {"Fiksi", "Non-Fiksi"};
+        pilihanFiksiAtauNon = new JComboBox(arrPilih1);
+        pnlPanel1.add(pilihanFiksiAtauNon);
+
+        lblLabelJenis = new JLabel("Jenis : ");
+        pnlPanel1.add(lblLabelJenis);
+        if (pilihanFiksiAtauNon.getSelectedItem().toString().equalsIgnoreCase("fiksi")) {
+            Object[] arrPilih2 = {"Novel", "Cerpen", "Puisi", "Komik"};
+            pilihanJenis = new JComboBox(arrPilih2);
+        } else {
+            Object[] arrPilih2 = {"Pelajaran", "Biografi", "Jurnal", "Berita"};
+            pilihanJenis = new JComboBox(arrPilih2);
+        }
+        pnlPanel1.add(pilihanJenis);
+
+        btnSubmit = new JButton("Submit");
+        btnSubmit.setPreferredSize(new Dimension(120, 20));
+        btnSubmit.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                onClickSubmit(e);
+            }
+        });
+        pnlPanel1.add(btnSubmit);
+
+        btnCancel = new JButton("Cancel");
+        btnCancel.setPreferredSize(new Dimension(120, 20));
+        btnCancel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                onClickCancel(e);
+            }
+        });
+        pnlPanel1.add(btnCancel);
+
+    }
+
+    public static void main(String[] args) {
+        new AddOrEdit().setVisible(true);
+    }
+
+    private JPanel pnlPanel1;
+    private JLabel lblLabelKodeBuku;
+    private JTextField txtTextFieldKodeBuku;
+    private JLabel lblLabelJudulBuku;
+    private JTextField txtTextFieldJudulBuku;
+    private JLabel lblLabelNamaPengarang;
+    private JTextField txtTextFieldNamaPengarang;
+    private JLabel lblLabelNamaPenerbit;
+    private JTextField txtTextFieldJNamaPenerbit;
+    private JLabel lblLabelTahunTerbit;
+    private JTextField txtTextFieldTahunTerbit;
+    private JLabel lblLabelFiksiAtauNon;
+    private JComboBox pilihanFiksiAtauNon;
+    private JLabel lblLabelJenis;
+    private JComboBox pilihanJenis;
+    private JButton btnSubmit;
+    private JButton btnCancel;
+}
---
+++ src/View/ListBuku.java
@@ -0,0 +1,108 @@
+package View;
+
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.WindowConstants;
+
+public class ListBuku extends JFrame {
+
+    public ListBuku() {
+        initComponents();
+    }
+
+    private void onClickSubmit(ActionEvent evt) {
+        if (evt.toString().equalsIgnoreCase("submit")) {
+            JOptionPane.showMessageDialog(null, "Berhasil di submit");
+        } else if (evt.toString().equalsIgnoreCase("edit")) {
+            JOptionPane.showMessageDialog(null, "Berhasil di edit");
+        } else {
+            JOptionPane.showMessageDialog(null, "Berhasil di cancel");
+        }
+    }
+
+    private void initComponents() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setSize(700, 400);
+        this.setLocationRelativeTo(null);
+
+        setTitle("List Buku");
+
+        getContentPane().setLayout(null);
+
+        pnlPanel1 = new JPanel();
+        pnlPanel1.setSize(700, 200);
+        add(pnlPanel1);
+
+        String[] arrTitle = {
+            "Kode Buku", "Nama Buku", "Nama Pengarang", "Nama Penerbit", "Tahun Terbit", "Fiksi/Non-Fiksi", "Jenis"
+        };
+
+        String[][] isiPerRow = {
+            {"01", "Pemrograman Java", "Alan S.", "Pustaka", "2010", "Non-Fiksi", "Pelajaran"},
+            {"02", "Pemrograman C", "Alan S.", "Pustaka", "2010", "Non-Fiksi", "Pelajaran"}
+        };
+
+        tblTabelList = new JTable(isiPerRow, arrTitle);
+
+        jSrcoll = new JScrollPane(tblTabelList);
+        tblTabelList.setFillsViewportHeight(true);
+        pnlPanel1.add(jSrcoll);
+
+        jSrcoll.setLocation(0, 0);
+        jSrcoll.setSize(new Dimension(200, 100));
+
+        pnlPanel2 = new JPanel();
+        pnlPanel2.setSize(700, 200);
+        add(pnlPanel2);
+
+        btnAdd = new JButton("Submit");
+        btnAdd.setPreferredSize(new Dimension(120, 20));
+        btnAdd.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                onClickSubmit(e);
+            }
+        });
+        pnlPanel2.add(btnAdd);
+
+        btnEdit = new JButton("Submit");
+        btnEdit.setPreferredSize(new Dimension(120, 20));
+        btnEdit.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                onClickSubmit(e);
+            }
+        });
+        pnlPanel2.add(btnEdit);
+
+        btnCancel = new JButton("Cancel");
+        btnCancel.setPreferredSize(new Dimension(120, 20));
+        btnCancel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                onClickSubmit(e);
+            }
+        });
+        pnlPanel2.add(btnCancel);
+    }
+
+    public static void main(String[] args) {
+        new ListBuku().setVisible(true);
+    }
+
+    private JPanel pnlPanel1;
+    private JTable tblTabelList;
+    private JScrollPane jSrcoll;
+    private JPanel pnlPanel2;
+    private JButton btnAdd;
+    private JButton btnEdit;
+    private JButton btnCancel;
+}
