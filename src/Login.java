af314615682f776d4f613bb10d6dba1cdd2c480d
jacobuscalvin <jacobus.calvin@gmail.com>
2018-07-16 22:22:11 +0700

    For Login

---
+++ src/Login.java
@@ -0,0 +1,18 @@
+
+import javax.swing.JComponent;
+
+/**
+ *
+ * @author JC Krisna
+ */
+public class Login {
+    
+    
+    
+    public static void main(String[] args) {
+        LoginFrame loginf = new LoginFrame();
+        loginf.setVisible(true);
+
+    }
+    
+}
