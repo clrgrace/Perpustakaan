c06de90204277e662e3dea7f648ce7e989e91b25
jacobuscalvin <jacobus.calvin@gmail.com>
2018-07-16 22:19:28 +0700

    Update
    + LoginFrame
    + MainMenuFrame

--- src/View/LoginFrame.java
+++ src/View/LoginFrame.java
@@ -1,4 +1,3 @@
-package View;
 
 import java.awt.Color;
 import java.awt.Font;
@@ -7,6 +6,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -27,11 +32,10 @@ public class LoginFrame extends JFrame {
     }
     
     private void initComponents(){
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         this.setSize(500, 250);
         this.setLocationRelativeTo(null);
         
-//        this.getContentPane().setBackground(new Color(183, 157, 73));
         this.setTitle("Perpustakaan");
         this.getContentPane().setLayout(null); // jpanel position
         
@@ -43,8 +47,8 @@ public class LoginFrame extends JFrame {
         add(pnl1);
         
         lblTitle = new JLabel("LOGIN PETUGAS");
-        lblTitle.setBounds(185, 1, 130, 30);
-        lblTitle.setFont(new Font("Arial",Font.BOLD,15));
+        lblTitle.setBounds(180, 1, 140, 30);
+        lblTitle.setFont(new Font("Arial",Font.BOLD,16));
         pnl1.add(lblTitle);
         
         // JPanel
@@ -78,20 +82,25 @@ public class LoginFrame extends JFrame {
         
         lblForgot = new JLabel("Forgot Password?");
         lblForgot.setBounds(250, 135, 100, 50);
-        lblForgot.setFont(new Font("SERIF",Font.PLAIN,12));
+        lblForgot.setFont(new Font("Serif",Font.PLAIN,12));
         pnl2.add(lblForgot);
         
         btnSubmit.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent ae) {
-                JOptionPane.showMessageDialog(null, "Welcome to Staff Page.");
+                onClickSubmit(ae);
             }
         });
         
         lblForgot.addMouseListener(new MouseListener() {
             @Override
             public void mouseClicked(MouseEvent me) {
-                JOptionPane.showInputDialog("Please Enter New Password..");
+                String id = JOptionPane.showInputDialog("Enter Id Petugas..");
+                String email = JOptionPane.showInputDialog("Enter Email Petugas..");
+//                String newPass = JOptionPane.showInputDialog("Please Enter New Password..");
+//                String newPassConfirm = JOptionPane.showInputDialog("Re-enter New Password..");
+                
+                ChangePassword(id, email);
             }
 
             @Override
@@ -110,12 +119,6 @@ public class LoginFrame extends JFrame {
             public void mouseExited(MouseEvent me) {
             }
         });
-        
-                
-    }
-    
-    public static void main(String[] args) {
-        new LoginFrame().setVisible(true);
     }
     
     private JPanel pnl1 = new JPanel();
@@ -128,4 +131,77 @@ public class LoginFrame extends JFrame {
     private JButton btnSubmit = new JButton();
     private JLabel lblForgot = new JLabel();
 
+    private void onClickSubmit(ActionEvent ev){
+        Connection con = null;
+        PreparedStatement pst = null;
+        ResultSet rs = null;    
+        
+        String username = txtUsername.getText() + "";
+        if(username.equals("")){
+            JOptionPane.showMessageDialog(null, "Please Enter Username");
+        }
+        else{
+            String sql = "SELECT * FROM petugas WHERE id_petugas=? AND password=?";
+            try{
+                 con = DriverManager.getConnection("jdbc:mysql://localhost/db_perpustakaan","root","");
+                 pst = con.prepareStatement(sql);
+                 pst.setString(1, txtUsername.getText());
+                 pst.setString(2, new String(txtPassword.getPassword()));
+                 
+                 rs = pst.executeQuery();
+                 if(rs.next()){
+                    dispose(); // untuk close
+                    JOptionPane.showMessageDialog(null, "Welcome, " + txtUsername.getText());
+                    MainMenuFrame mmFrame = new MainMenuFrame();
+                    mmFrame.setVisible(true);
+                 } 
+                 else{
+                    JOptionPane.showMessageDialog(null, "Wrong Username or Password");
+                 }
+            } 
+            catch(SQLException ex){
+                JOptionPane.showMessageDialog(null, ex);
+            }
+        }
+    }
+    
+    public void ChangePassword(String id, String em){
+        Connection con2 = null;
+        PreparedStatement pst2 = null;
+        ResultSet rs2 = null; 
+        
+        String sql = "SELECT * FROM petugas WHERE id_petugas=? AND email=?";
+        try{
+            con2 = DriverManager.getConnection("jdbc:mysql://localhost/db_perpustakaan","root","");
+            pst2 = con2.prepareStatement(sql);
+            pst2.setString(1, id);
+            pst2.setString(2, em);
+
+            rs2 = pst2.executeQuery();
+            if(rs2.next()){
+                String newPass = JOptionPane.showInputDialog("Please Enter New Password..");
+                String newPassConfirm = JOptionPane.showInputDialog("Re-enter New Password..");
+                
+//                if(newPass != newPassConfirm){
+                if(newPass.equals(newPassConfirm)){
+                    String sql2 = "UPDATE petugas SET password=? WHERE id_petugas=?";
+                    con2 = DriverManager.getConnection("jdbc:mysql://localhost/db_perpustakaan","root","");
+                    pst2 = con2.prepareStatement(sql2);
+                    pst2.setString(1, newPassConfirm);
+                    pst2.setString(2, id);
+                    
+                    pst2.execute();
+                    JOptionPane.showMessageDialog(null, "Change Password Success!");
+                } else{
+                    JOptionPane.showMessageDialog(null, "Wrong Password Confirmation!");
+                }
+            } 
+            else{
+               JOptionPane.showMessageDialog(null, "Petugas Not Found!");
+            }
+        } 
+        catch(SQLException ex){
+            JOptionPane.showMessageDialog(null, ex);
+        }
+    }
 }
--- src/View/MainMenuFrame.java
+++ src/View/MainMenuFrame.java
@@ -1,16 +1,21 @@
-package View;
 
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashSet;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.WindowConstants;
 
 /**
@@ -33,7 +38,7 @@ public class MainMenuFrame extends JFrame{
         this.getContentPane().setLayout(null); // jpanel position
     
         lblUser = new JLabel("Welcome to Library, ");
-        lblUser.setBounds(35, 2, 200, 100);
+        lblUser.setBounds(35, 2, 400, 100);
         lblUser.setFont(new Font("Arial",Font.BOLD,15));
         add(lblUser);
         
@@ -46,9 +51,21 @@ public class MainMenuFrame extends JFrame{
         btnLogout.setBounds(750, 25, 100, 50); // x, y, width, height
         add(btnLogout);
         
+        btnLogout.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                int r = JOptionPane.showConfirmDialog(null, "Are you sure? ", "Logout", JOptionPane.YES_NO_OPTION);
+                if(r == JOptionPane.YES_OPTION){
+                    JOptionPane.showMessageDialog(null, "Thankyou for using this program");
+                    dispose();
+                }
+            }
+        });
+        
         lblIcon1 = new JLabel();
-        lblIcon1.setBounds(90, 200, 200, 200);
+        lblIcon1.setBounds(90, 250, 80, 90);
         lblIcon1.setIcon(new ImageIcon(resizeImage("icon/a.png")));
+        lblIcon1.setToolTipText("<HTML><H3>Add Book<H3></HTML>");
         add(lblIcon1);
         
         lblDesc1 = new JLabel("Add Book");
@@ -56,9 +73,33 @@ public class MainMenuFrame extends JFrame{
         lblDesc1.setFont(new Font("Britannic Bold",Font.BOLD,20));
         add(lblDesc1);
         
+        lblIcon1.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent me) {
+                JOptionPane.showMessageDialog(null, "Please Input Book Name");
+            }
+
+            @Override
+            public void mousePressed(MouseEvent me) {
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent me) {
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent me) {
+            }
+
+            @Override
+            public void mouseExited(MouseEvent me) {
+            }
+        });
+        
         lblIcon2= new JLabel();
-        lblIcon2.setBounds(250, 200, 200, 200);
+        lblIcon2.setBounds(250, 255, 80, 90);
         lblIcon2.setIcon(new ImageIcon(resizeImage("icon/b.png")));
+        lblIcon2.setToolTipText("<HTML><H3>Register<H3></HTML>");
         add(lblIcon2);
         
         lblDesc2 = new JLabel("Register");
@@ -66,9 +107,33 @@ public class MainMenuFrame extends JFrame{
         lblDesc2.setFont(new Font("Britannic Bold",Font.BOLD,21));
         add(lblDesc2);
         
+        lblIcon2.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent me) {
+                JOptionPane.showMessageDialog(null, "Please Input Person's Name");
+            }
+
+            @Override
+            public void mousePressed(MouseEvent me) {
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent me) {
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent me) {
+            }
+
+            @Override
+            public void mouseExited(MouseEvent me) {
+            }
+        });
+        
         lblIcon3 = new JLabel();
-        lblIcon3.setBounds(400, 200, 200, 200);
+        lblIcon3.setBounds(400, 250, 80, 90);
         lblIcon3.setIcon(new ImageIcon(resizeImage("icon/c.png")));
+        lblIcon3.setToolTipText("<HTML><H3>Borrow a book<H3></HTML>");
         add(lblIcon3);
         
         lblDesc3 = new JLabel("Borrow Book");
@@ -76,9 +141,33 @@ public class MainMenuFrame extends JFrame{
         lblDesc3.setFont(new Font("Britannic Bold",Font.BOLD,20));
         add(lblDesc3);
         
+        lblIcon3.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent me) {
+                JOptionPane.showMessageDialog(null, "Please Input Date of Borrows");
+            }
+
+            @Override
+            public void mousePressed(MouseEvent me) {
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent me) {
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent me) {
+            }
+
+            @Override
+            public void mouseExited(MouseEvent me) {
+            }
+        });
+        
         lblIcon4 = new JLabel();
-        lblIcon4.setBounds(560, 200, 200, 200);
+        lblIcon4.setBounds(560, 250, 80, 90);
         lblIcon4.setIcon(new ImageIcon(resizeImage("icon/d.png")));
+        lblIcon4.setToolTipText("<HTML><H3>Return Book<H3></HTML>");
         add(lblIcon4);
         
         lblDesc4 = new JLabel("Return Book");
@@ -86,19 +175,63 @@ public class MainMenuFrame extends JFrame{
         lblDesc4.setFont(new Font("Britannic Bold",Font.BOLD,20));
         add(lblDesc4);
         
+        lblIcon4.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent me) {
+                JOptionPane.showMessageDialog(null, "Please Input Date of Returns");
+            }
+
+            @Override
+            public void mousePressed(MouseEvent me) {
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent me) {
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent me) {
+            }
+
+            @Override
+            public void mouseExited(MouseEvent me) {
+            }
+        });
+        
         lblIcon5 = new JLabel();
-        lblIcon5.setBounds(710, 200, 200, 200);
+        lblIcon5.setBounds(710, 250, 80, 90);
         lblIcon5.setIcon(new ImageIcon(resizeImage("icon/e.png")));
+        lblIcon5.setToolTipText("<HTML><H3>History<H3></HTML>");
         add(lblIcon5);
         
         lblDesc5 = new JLabel("History");
         lblDesc5.setBounds(710, 262, 200, 200);
         lblDesc5.setFont(new Font("Britannic Bold",Font.BOLD,20));
         add(lblDesc5);
-    }
-    
-    public static void main(String[] args) {
-        new MainMenuFrame().setVisible(true);
+        
+        lblIcon5.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent me) {
+                JOptionPane.showMessageDialog(null, "This is the History Page");
+            }
+
+            @Override
+            public void mousePressed(MouseEvent me) {
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent me) {
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent me) {
+            }
+
+            @Override
+            public void mouseExited(MouseEvent me) {
+            }
+        });
+        
     }
     
     private JLabel lblUser = new JLabel();
@@ -120,9 +253,11 @@ public class MainMenuFrame extends JFrame{
         try{
             BufferedImage img = ImageIO.read(new File(url));
             dimg = img.getScaledInstance(75, 75, Image.SCALE_SMOOTH);
+            
         } catch(IOException ex){
             ex.printStackTrace(System.err);
         }
         return dimg;
     }
+    
 }
