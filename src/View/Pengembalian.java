f883b296b54ac536c3a55b5e1d9d13adec3344b4
samuelzz01 <giniro.tamashi@gmail.com>
2018-07-18 13:48:29 +0700

    edit gui samuel

--- src/View/Pengembalian.java
+++ src/View/Pengembalian.java
@@ -1,11 +1,13 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package View;
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
+package UI;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
 /**
@@ -25,7 +27,7 @@ class Pengembalian extends JFrame {
     }
     
     void initComponent(){
-        setSize(300,300);
+        setSize(300,100);
         setLocationRelativeTo(null);
         
         panel1 = new JPanel();
@@ -38,6 +40,26 @@ class Pengembalian extends JFrame {
         panel1.add(txt1pnl1);
         
         panel2 = new JPanel();
+        button1pnl1.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                Function.Function_PengembalianBuku pengembalian = new Function.Function_PengembalianBuku();
+                Model.Buku buku = pengembalian.Pengembalian(txt1pnl1.getText());
+                if(buku == null){
+                    JOptionPane.showMessageDialog(null,"Kode Buku Tidak Ada");
+                }
+                else{
+                    KonfirmasiPengembalian confirm = new KonfirmasiPengembalian(buku);
+                }
+            }
+        });
+        button2pnl1.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                
+                dispose();
+            }
+        });
         panel2.add(button1pnl1);
         panel2.add(button2pnl1);
         add(panel1);
