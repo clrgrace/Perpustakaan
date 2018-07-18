ed49143f4082f96bd530456d36e235056bf09c03
Grace Vanessa <graceace49@gmail.com>
2018-07-18 14:09:42 +0700

    (ubah destinasi package - 2)

--- src/View/KonfirmasiPengembalian.java
+++ src/View/KonfirmasiPengembalian.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package UI;
+package View;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
