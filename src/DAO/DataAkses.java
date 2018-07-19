db73a17c43c45ccfbfd923d16f920f030963d35e
jacobuscalvin <jacobus.calvin@gmail.com>
2018-07-19 21:35:53 +0700

    Rapihin DataAkses

--- src/DAO/DataAkses.java
+++ src/DAO/DataAkses.java
@@ -170,6 +170,68 @@ public class DataAkses {
         return listBukuFiksi;
     }
     
+    public static void deleteBuku(int kode){
+        
+        String query = "DELETE FROM buku WHERE kode_buku = ?";
+        try{
+            Connection con5 = ConnectionManager.getConnection();
+            PreparedStatement st5 = con5.prepareStatement(query);
+            st5.setInt(1, kode);
+            
+            st5.execute();
+            JOptionPane.showMessageDialog(null, "Delete Sukses!");
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+    
+    public static void updateBuku(int kode){
+        
+        String query = "SELECT * FROM buku WHERE kode_buku = ?";
+        try{
+            Connection con6 = ConnectionManager.getConnection();
+            PreparedStatement st6 = con6.prepareStatement(query);
+            st6.setInt(1, kode);
+            
+            ResultSet rs6 = st6.executeQuery();
+            if(rs6.next()){
+                String jdl = JOptionPane.showInputDialog(null,"Input Judul Buku : ");
+                String pgr = JOptionPane.showInputDialog(null, "Input Pengarang : ");
+                String pbt = JOptionPane.showInputDialog(null, "Input Penerbit : ");
+                int tT = Integer.parseInt(JOptionPane.showInputDialog(null, "Input Tahun Terbit : "));
+                String jenis = JOptionPane.showInputDialog(null, "Fiksi / NonFiksi (Text must Appropriate)");
+                String sps = JOptionPane.showInputDialog(null, "Spesifikasi Buku");
+                String stat = JOptionPane.showInputDialog(null, "Available (Y / N)");
+                
+                String query2 = "UPDATE buku SET judul_buku=?, nama_pengarang=?, nama_penerbit=?, tahun_terbit=?, jenis_buku=?, spesifikasi_buku=?, available=? WHERE kode_buku=?";
+                    PreparedStatement st7 = con6.prepareStatement(query2);
+                    st7.setString(1, jdl);
+                    st7.setString(2, pgr);
+                    st7.setString(3, pbt);
+                    st7.setInt(4, tT);
+                    st7.setString(5, jenis);
+                    st7.setString(6, sps);
+                    st7.setString(7, stat);
+                    st7.setInt(8, kode);
+                    
+                    st7.execute();
+                    JOptionPane.showMessageDialog(null, "Update Buku Sukses!");
+            }
+            else{
+                JOptionPane.showMessageDialog(null, "Book not found!");
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    // SAMUEL
     public static List<BukuFiksi> showBuku(String x,String y){
         List <BukuFiksi> listBukuFiksi = new ArrayList<>();
         
@@ -230,62 +292,6 @@ public class DataAkses {
             Logger.getLogger(DataAkses.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
-    
-    public static void deleteBuku(int kode){
-        
-        String query = "DELETE FROM buku WHERE kode_buku = ?";
-        try{
-            Connection con5 = ConnectionManager.getConnection();
-            PreparedStatement st5 = con5.prepareStatement(query);
-            st5.setInt(1, kode);
-            
-            st5.execute();
-            JOptionPane.showMessageDialog(null, "Delete Sukses!");
-        } catch (SQLException e){
-            e.printStackTrace();
-        }
-    }
-    
-    public static void updateBuku(int kode){
-        
-        String query = "SELECT * FROM buku WHERE kode_buku = ?";
-        try{
-            Connection con6 = ConnectionManager.getConnection();
-            PreparedStatement st6 = con6.prepareStatement(query);
-            st6.setInt(1, kode);
-            
-            ResultSet rs6 = st6.executeQuery();
-            if(rs6.next()){
-                String jdl = JOptionPane.showInputDialog(null,"Input Judul Buku : ");
-                String pgr = JOptionPane.showInputDialog(null, "Input Pengarang : ");
-                String pbt = JOptionPane.showInputDialog(null, "Input Penerbit : ");
-                int tT = Integer.parseInt(JOptionPane.showInputDialog(null, "Input Tahun Terbit : "));
-                String jenis = JOptionPane.showInputDialog(null, "Fiksi / NonFiksi (Text must Appropriate)");
-                String sps = JOptionPane.showInputDialog(null, "Spesifikasi Buku");
-                String stat = JOptionPane.showInputDialog(null, "Available (Y / N)");
-                
-                String query2 = "UPDATE buku SET judul_buku=?, nama_pengarang=?, nama_penerbit=?, tahun_terbit=?, jenis_buku=?, spesifikasi_buku=?, available=? WHERE kode_buku=?";
-                    PreparedStatement st7 = con6.prepareStatement(query2);
-                    st7.setString(1, jdl);
-                    st7.setString(2, pgr);
-                    st7.setString(3, pbt);
-                    st7.setInt(4, tT);
-                    st7.setString(5, jenis);
-                    st7.setString(6, sps);
-                    st7.setString(7, stat);
-                    st7.setInt(8, kode);
-                    
-                    st7.execute();
-                    JOptionPane.showMessageDialog(null, "Update Buku Sukses!");
-            }
-            else{
-                JOptionPane.showMessageDialog(null, "Book not found!");
-            }
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
-    }
         
     public static void deleteTranksaksi(int kode){
         
@@ -299,6 +305,11 @@ public class DataAkses {
             e.printStackTrace();
         }
     }
+    
+    
+    
+    
+    
     
     //GRACE
     public static void addDosen(Model.Dosen dosen){
